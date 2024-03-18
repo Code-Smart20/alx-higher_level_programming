9a7f8007c96de99114d25a1af0a9839ebd8f5b22
nwachukwu ikedinachi <bldrnwagod@gmail.com>
2024-03-18 07:42:22 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     newstr = my_list[:]
     if idx < 0:
         return (newstr)
-    if idx > len(my-list) - 1:
+    if idx > len(my_list) - 1:
         return (newstr)
     newstr[idx] = element
     return (newstr]
