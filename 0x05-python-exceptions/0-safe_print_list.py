56dbd523a33070ed791ae549db67983d92b482ce
root <you@example.com>
2024-06-25 04:35:20 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
     try:
-        for i in range(x):
-            print("{}".format(my_list[i], end=''))
+        for item in range(x):
+            print("{}".format(my_list[i]), end=''
             num = num + 1
     except IndexError:
         pass
